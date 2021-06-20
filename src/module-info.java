c0755344bb4195ab76c2bae6429fb03d5a4fcae3
Gandharthus <72447580+Gandharthus@users.noreply.github.com>
2021-06-20 22:56:28 +0100

    Test variable , sql file added

--- src/module-info.java
+++ src/module-info.java
@@ -1,7 +1,9 @@
 module MiniProjetJava {
 	requires javafx.controls;
 	requires java.sql;
+	requires javafx.fxml;
 	
+	opens Controller to javafx.fxml;
 	opens entities to java.sql,java.util;
 	opens application to javafx.graphics, javafx.fxml;
 }
